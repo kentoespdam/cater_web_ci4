fdb03e5f19b11effb0366f409405da02d6a45d8d
kentoespdam <kentoes.pdam@gmail.com>
2024-05-13 10:05:50 +0700

    move asset file

--- app/Views/template/index.php
+++ app/Views/template/index.php
@@ -21,10 +21,10 @@
     <link rel="stylesheet" href="<?= base_url('assets') ?>/ionicons/css/ionicons.min.css">
 
     <!-- Theme style -->
-    <link rel="stylesheet" href="<?= base_url('assets') ?>/dist/css/AdminLTE.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets') ?>/css/AdminLTE.min.css">
     <!-- AdminLTE Skins. Choose a skin from the css/skins
     folder instead of downloading all of them to reduce the load. -->
-    <link rel="stylesheet" href="<?= base_url('assets') ?>/dist/css/skins/_all-skins.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets') ?>/css/skins/_all-skins.min.css">
     <!-- iCheck -->
     <!-- <link rel="stylesheet" href="<?= base_url('assets') ?>/plugins/iCheck/flat/blue.css">
     -->
