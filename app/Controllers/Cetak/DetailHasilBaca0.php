179f42d6c4a7af30d94b7400a5f3d10aadf3bb26
kentoespdam <kentoes.pdam@gmail.com>
2024-06-03 13:52:39 +0700

    update export excel detail hasil baca 0

--- app/Controllers/Cetak/DetailHasilBaca0.php
+++ app/Controllers/Cetak/DetailHasilBaca0.php
@@ -52,6 +52,9 @@ class DetailHasilBaca0 extends BaseController
         ini_set('memory_limit', '-1');
         $spreadsheet = new Spreadsheet();
         $sheet = $spreadsheet->setActiveSheetIndex(0);
+        $sheet->setTitle('Dettail Kondisi Water Meter');
+        $sheet->getPageSetup()->setOrientation(\PhpOffice\PhpSpreadsheet\Worksheet\PageSetup::ORIENTATION_LANDSCAPE);
+
 
         foreach ($this->fields as $field) {
             $f = (object)$field;
@@ -60,27 +63,11 @@ class DetailHasilBaca0 extends BaseController
 
         $rowNum = 2;
         foreach ($data as $row) {
-            $sheet->setCellValue("A" . $rowNum, $row->tgl);
-            $sheet->setCellValue("B" . $rowNum, $row->no_sam);
-            $sheet->setCellValue("C" . $rowNum, $row->stan_kini);
-            $sheet->setCellValue("D" . $rowNum, $row->stan_lalu);
-            $sheet->setCellValue("E" . $rowNum, $row->pakai);
-            $sheet->setCellValue("F" . $rowNum, $row->petugas);
-            $sheet->setCellValue("G" . $rowNum, $row->kondisi);
-            $sheet->setCellValue("H" . $rowNum, $row->ket);
-            $sheet->setCellValue("I" . $rowNum, $row->info);
-            $sheet->setCellValue("J" . $rowNum, $row->ptgs_met);
-            $sheet->setCellValue("K" . $rowNum, $row->rata);
-            $sheet->setCellValue("L" . $rowNum, $row->kd_wil);
-            $sheet->setCellValue("M" . $rowNum, $row->wil);
-            $sheet->setCellValue("N" . $rowNum, $row->kd_cabang);
-            $sheet->setCellValue("O" . $rowNum, $row->nm_cabang);
-            $sheet->setCellValue("P" . $rowNum, $row->kd_cek);
-            $sheet->setCellValue("Q" . $rowNum, $row->status_cek);
-            $sheet->setCellValue("R" . $rowNum, $row->nama);
-            $sheet->setCellValue("S" . $rowNum, $row->alamat);
+            $this->setSheetValue($sheet, $row, $rowNum);
             $rowNum++;
         }
+
+
 
         $writer = new Xlsx($spreadsheet);
         $filename = "hasil_baca_0-" . date('Y-m-d-His');
@@ -90,5 +77,30 @@ class DetailHasilBaca0 extends BaseController
         header('Cache-Control: max-age=0');
 
         $writer->save('php://output');
+        $spreadsheet->disconnectWorksheets();
+        unset($spreadsheet);
+    }
+
+    private function setSheetValue($sheet, object $row, $rowNum)
+    {
+        $sheet->setCellValue("A" . $rowNum, $row->tgl);
+        $sheet->setCellValue("B" . $rowNum, $row->no_sam);
+        $sheet->setCellValue("C" . $rowNum, $row->stan_kini);
+        $sheet->setCellValue("D" . $rowNum, $row->stan_lalu);
+        $sheet->setCellValue("E" . $rowNum, $row->pakai);
+        $sheet->setCellValue("F" . $rowNum, $row->petugas);
+        $sheet->setCellValue("G" . $rowNum, $row->kondisi);
+        $sheet->setCellValue("H" . $rowNum, $row->ket);
+        $sheet->setCellValue("I" . $rowNum, $row->info);
+        $sheet->setCellValue("J" . $rowNum, $row->ptgs_met);
+        $sheet->setCellValue("K" . $rowNum, $row->rata);
+        $sheet->setCellValue("L" . $rowNum, $row->kd_wil);
+        $sheet->setCellValue("M" . $rowNum, $row->wil);
+        $sheet->setCellValue("N" . $rowNum, $row->kd_cabang);
+        $sheet->setCellValue("O" . $rowNum, $row->nm_cabang);
+        $sheet->setCellValue("P" . $rowNum, $row->kd_cek);
+        $sheet->setCellValue("Q" . $rowNum, $row->status_cek);
+        $sheet->setCellValue("R" . $rowNum, $row->nama);
+        $sheet->setCellValue("S" . $rowNum, $row->alamat);
     }
 }
